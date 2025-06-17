d7fc7923526630f8675e297c96c4d26f448c4432
Mati Joseph <crucialniccur@gmail.com>
2025-06-17 14:17:48 +0300

    Add initial PostgreSQL setup and create models for pizzas, restaurants, and restaurant_pizzas

--- migrations/versions/3049044fd880_initial_postgres_setup.py
+++ migrations/versions/3049044fd880_initial_postgres_setup.py
@@ -1,8 +1,8 @@
-"""Add Restaurant, Pizza, RestaurantPizza models
+"""initial postgres setup
 
-Revision ID: 3de0b35ab512
+Revision ID: 3049044fd880
 Revises: 
-Create Date: 2025-06-17 09:00:03.613941
+Create Date: 2025-06-17 14:13:20.952157
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '3de0b35ab512'
+revision = '3049044fd880'
 down_revision = None
 branch_labels = None
 depends_on = None
