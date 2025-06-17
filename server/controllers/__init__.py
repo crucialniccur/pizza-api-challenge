9d0b9ff9411beb62c1cdba74c788bb00a24c2235
Mati Joseph <crucialniccur@gmail.com>
2025-06-17 08:33:40 +0300

    clean slate two

--- server/controllers/__init__.py
+++
@@ -1,3 +0,0 @@
-from .restaurant_controller import restaurant_bp
-from .pizza_controller import pizza_bp
-from .restaurant_pizza_controller import restaurant_pizza_bp
--- server/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from .restaurant import Restaurant
-from .pizza import Pizza
-from .restaurant_pizza import RestaurantPizza
