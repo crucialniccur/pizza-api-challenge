f95307c962302972938ef8932e7753aedaa2994a
Mati Joseph <crucialniccur@gmail.com>
2025-06-17 05:41:22 +0300

    Add POST endpoint to create restaurant-pizza associations

---
+++ server/controllers/restaurant_pizza_controller.py
@@ -0,0 +1,29 @@
+from flask import Blueprint, jsonify, request
+from config import db
+from models.restaurant_pizza import RestaurantPizza
+from models.restaurant import Restaurant
+from models.pizza import Pizza
+from sqlalchemy.exc import IntegrityError
+
+restaurant_pizza_bp = Blueprint('restaurant_pizza_bp', __name__)
+
+
+@restaurant_pizza_bp.route('/restaurant_pizzas', methods=['POST'])
+def create_restaurant_pizza():
+    data = request.get_json()
+    try:
+        restaurant_pizza = RestaurantPizza(
+            price=data['price'],
+            pizza_id=data['pizza_id'],
+            restaurant_id=data['restaurant_id']
+        )
+        db.session.add(restaurant_pizza)
+        db.session.commit()
+        response = restaurant_pizza.to_dict()
+        response['pizza'] = Pizza.query.get(data['pizza_id']).to_dict()
+        response['restaurant'] = Restaurant.query.get(
+            data['restaurant_id']).to_dict()
+        return jsonify(response), 201
+    except (ValueError, IntegrityError) as e:
+        db.session.rollback()
+        return jsonify({'errors': [str(e)]}), 400
