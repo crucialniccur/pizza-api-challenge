325321945cb6b659840f316b7d2113ca9ae737bb
Mati Joseph <crucialniccur@gmail.com>
2025-06-17 08:51:03 +0300

    Move database to project root

--- server/config.py
+++ server/config.py
@@ -2,5 +2,7 @@ import os
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///pizza_restaurant.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + \
+        os.path.join(os.path.dirname(os.path.dirname(__file__)),
+                     'pizza_restaurant.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
